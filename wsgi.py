3f1351c71a10db5e9fa4c48725f27d526cf415fc
nsetzer <nsetzer@cogitocorp.com>
2017-12-22 16:04:38 -0500

    add basic flask app

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+
+# https://github.com/openshift-quickstart/flask-example
+# https://github.com/openshift/django-ex
+
+# https://github.com/OpenShiftDemos/os-sample-python
+# https://github.com/eleanordare/flask-python-openshift
+
+import os
+from server.flaskapp import app as application
+
+
